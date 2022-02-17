def892ea25f85322290e3398507aab24e40e7f8f
shyamalasrikaunth <shyamala@arraycom.co.in>
2022-02-17 17:49:09 +0530

    1702

--- Js/main.js
+++ Js/main.js
@@ -1,55 +1,58 @@
 $(window).on('load', function() {
     $("#pre-load").delay(1500).fadeOut(1000);
 });
+
 $(function () {
-    var lastScrollTop = 0;
-    var $topbar = $('.top-bar');
-    var $navbar = $('.navbar');
-    var $logo1 = $('.logo1');
-    var $logo2 = $('.logo2');
-  
-    $(window).scroll(function(event){
-      var st = $(this).scrollTop();
-      
-      if (st > 1) { // scroll down
-        
-        // use this is jQuery full is used
-        $topbar.fadeOut()
-        $navbar.addClass('sticky');
-        $navbar.removeClass('bg-light');
-        $navbar.addClass('bg-menucolor')
-         $logo1.addClass('d-none');
-         $logo1.removeClass('d-block');
-         $logo2.removeClass('d-none');
-         $logo2.addClass('d-block');
-         $(".dot").css("background", "white");
-        // use this to use CSS3 animation
-        // $navbar.addClass("fade-out");
-        // $navbar.removeClass("fade-in");
-        
-        // use this if no effect is required
-        // $navbar.hide();
-      } else { // scroll up
-          
-        // use this is jQuery full is used
-        $topbar.fadeIn();
-        $navbar.removeClass('sticky');
-        $navbar.removeClass('bg-menucolor');
-        $navbar.addClass('bg-light')
-        $logo1.addClass('d-block');
-         $logo2.removeClass('d-block');
-         $logo2.addClass('d-none');
-         $(".dot").css("background", "#bb0b0b");
-        // use this to use CSS3 animation
-        // $navbar.addClass("fade-in");
-        // $navbar.removeClass("fade-out");
-        
-        // use this if no effect is required
-        // $navbar.show();
-      }
-      lastScrollTop = st;
-    });
-  });
+var lastScrollTop = 0;
+var $topbar = $('.top-bar');
+var $navbar = $('.navbar');
+var $logo1 = $('.logo1');
+var $logo2 = $('.logo2');
+
+$(window).scroll(function(event){
+var st = $(this).scrollTop();
+var footerOffset = $('#footer').offset().top;
+ 
+if (st >  1) { // scroll down
+
+// use this is jQuery full is used
+$topbar.fadeOut()
+$navbar.addClass('sticky');
+$navbar.removeClass('bg-light');
+$navbar.addClass('bg-menucolor')
+$logo1.addClass('d-none');
+$logo1.removeClass('d-block');
+$logo2.removeClass('d-none');
+$logo2.addClass('d-block');
+$(".dot").css("background", "white");
+// use this to use CSS3 animation
+// $navbar.addClass("fade-out");
+// $navbar.removeClass("fade-in");
+
+// use this if no effect is required
+// $navbar.hide();
+} else { // scroll up
+ 
+// use this is jQuery full is used
+$topbar.fadeIn();
+$navbar.removeClass('sticky');
+$navbar.removeClass('bg-menucolor');
+$navbar.addClass('bg-light')
+$logo1.addClass('d-block');
+$logo2.removeClass('d-block');
+$logo2.addClass('d-none');
+$(".dot").css("background", "#bb0b0b");
+// use this to use CSS3 animation
+// $navbar.addClass("fade-in");
+// $navbar.removeClass("fade-out");
+
+// use this if no effect is required
+// $navbar.show();
+}
+lastScrollTop = st;
+});
+});
+ 
   
   $('navbar-nav').on('mouseover', 'nav-item', function() {
     $('li.active').removeClass('active');
